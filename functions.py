2fead418c307899f81e9ad6cd17ed7e94be094f2
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2022-03-12 11:14:30 -0300

    Two monitors approach

---
+++ functions.py
@@ -0,0 +1,144 @@
+import re
+import subprocess
+import time
+
+import tkinter as tk
+from screeninfo import get_monitors
+
+
+def get_active_window_title():
+    root = subprocess.Popen(['xprop', '-root', '_NET_ACTIVE_WINDOW'], stdout=subprocess.PIPE)
+    stdout, stderr = root.communicate()
+    m = re.search(b'^_NET_ACTIVE_WINDOW.* ([\w]+)$', stdout)
+    if m is None:
+        return None
+    window_id = m.group(1)
+    window = subprocess.Popen(['xprop', '-id', window_id, 'WM_NAME'], stdout=subprocess.PIPE)
+    stdout, stderr = window.communicate()
+    match = re.match(b"WM_NAME\(\w+\) = (?P<name>.+)$", stdout)
+    if match is None:
+        return None
+    return match.group("name").strip(b'"').decode()
+
+
+def _execute_command(command):
+    process = subprocess.Popen(command.split(" "), stdout=subprocess.PIPE)
+    (output, err) = process.communicate()
+    process.wait()
+    return list(filter(lambda line: line, output.decode().split('\n')))
+
+
+def get_window():
+    window_name = get_active_window_title()
+    for line in _execute_command("wmctrl -p -G -l"):
+        values = list(filter(lambda i: i, line.split(' ')))
+        if len(values) > 6:
+            name = ' '.join(values[8:])
+            if window_name == name:
+                return values[0]
+
+
+def get_screens(columns, lines):
+    app = tk.Tk()
+    roots = []
+    screens = []
+    for monitor in get_monitors():
+        blocks = []
+        root = tk.Toplevel(app)
+        root.geometry("{}x{}+{}+{}".format(monitor.width, monitor.height, monitor.x, monitor.y))
+        root.overrideredirect(True)
+        root.wait_visibility(root)
+        root.wm_attributes("-alpha", 0.5)
+        canvas = tk.Canvas(root, width=monitor.width, height=monitor.height)
+        canvas.pack()
+        for w_index in range(0, columns):
+            for h_index in range(0, lines):
+                size_column = monitor.width / columns
+                size_line = monitor.height / lines
+                init_x = monitor.x + size_column * w_index
+                end_x = init_x + size_column
+                init_y = monitor.y + size_line * h_index
+                end_y = init_y + size_line
+                blocks.append([init_x, end_x, init_y, end_y,
+                               canvas.create_rectangle(init_x-monitor.x, init_y-monitor.y, end_x-monitor.x, end_y-monitor.y, fill="blue")])
+            roots.append(root)
+        screens.append({
+            'blocks': blocks,
+            'canvas': canvas
+        })
+    return app, roots, screens
+
+
+def on_move(x, y, buttons, keyboard_keys, screens, last_x, last_y, last_command, last_timestamp, last_window,
+            gui_roots):
+    ctrl = len(list(filter(lambda n: n == 'ctrl', keyboard_keys))) > 0
+    right_clicked = len(list(filter(lambda bb: bb.value == 3, buttons))) == 1
+    if not ctrl or not right_clicked:
+        for root in gui_roots:
+            root.withdraw()
+        return
+    for root in gui_roots:
+        root.deiconify()
+    if time.time() - last_timestamp < 0.3:
+        return
+    last_timestamp = time.time()
+    for screen in screens:
+        for block in screen['blocks']:
+            init_x, end_x, init_y, end_y, rectangle = block
+            if init_x <= x <= end_x and init_y <= y <= end_y:
+                left_clicked = len(list(filter(lambda bb: bb.value == 1, buttons))) == 1
+                window = get_window()
+                if screen['canvas'].itemconfig(rectangle)['fill'] != 'green':
+                    screen['canvas'].itemconfig(rectangle, fill='green')
+                if left_clicked:
+                    window = get_window()
+                    command = "wmctrl -i -r " + window + " -e 0," + str(int(last_x)) + "," + str(
+                        int(last_y)) + "," + str(
+                        int(end_x - last_x)) + "," + str(int(end_y - last_y))
+                    if last_command == command:
+                        continue
+                    if not last_window or last_window != window:
+                        _execute_command("wmctrl -i -r " + window + " -b remove,maximized_vert,maximized_horz")
+                        last_window = last_window
+                    _execute_command(command)
+                else:
+                    last_x = init_x
+                    last_y = init_y
+                    command = "wmctrl -i -r " + window + " -e 0," + str(int(last_x)) + "," + str(
+                        int(last_y)) + "," + str(
+                        int(end_x - init_x)) + "," + str(int(end_y - init_y))
+                    if last_command == command:
+                        continue
+                    last_command = command
+                    if not last_window or last_window != window:
+                        _execute_command("wmctrl -i -r " + window + " -b remove,maximized_vert,maximized_horz")
+                        last_window = last_window
+                    _execute_command(command)
+                continue
+            else:
+                if screen['canvas'].itemconfig(rectangle)['fill'] != 'blue':
+                    screen['canvas'].itemconfig(rectangle, fill='blue')
+    return last_x, last_y, last_command, last_timestamp, last_window
+
+
+def on_click(_x, _y, button, pressed, buttons):
+    if pressed:
+        buttons.append(button)
+        return buttons
+    return list(filter(lambda _button: _button.value != button.value, buttons))
+
+
+def on_press(key, keyboard_keys):
+    try:
+        if key not in keyboard_keys:
+            keyboard_keys.append(key.name)
+        return keyboard_keys
+    except:
+        return keyboard_keys
+
+
+def on_release(key, keyboard_keys):
+    try:
+        return list(filter(lambda k: k != key.name, keyboard_keys))
+    except:
+        return keyboard_keys
--- pywindowgrid.py
+++ pywindowgrid.py
@@ -1,163 +1,59 @@
-import re
-import subprocess
 import threading
-from subprocess import Popen, PIPE
+import time
 
-keys = []
+from functions import get_screens, on_click, on_move, on_press, on_release
+
 buttons = []
-lock_x = 0
-lock_y = 0
+keyboard_keys = []
 
-columns = 10
-lines = 5
+gui_app, gui_roots, screens = get_screens(10, 5)
 
-
-def _execute_command(command):
-    process = Popen(command.split(" "), stdout=PIPE)
-    (output, err) = process.communicate()
-    process.wait()
-    return list(filter(lambda line: line, output.decode().split('\n')))
+last_timestamp = time.time()
+last_x, last_y, last_command, last_window = None, None, None, None
 
 
-avaliable_size_response = _execute_command('xprop -root _NET_WORKAREA')
-avaliable_size = re.split('=|,', avaliable_size_response[0])[1:5]
-init_x = int(avaliable_size[0])
-init_y = int(avaliable_size[1])
-width = int(avaliable_size[2])
-height = int(avaliable_size[3])
-block_size_w = width / columns
-block_size_h = height / lines
-
-screens = []
-screens_lines = _execute_command("xrandr --listmonitors")
-for s_line in screens_lines:
-    parts = s_line.split(' ')
-    if s_line and len(parts) >= 4:
-        size_position = parts[3]
-        size, x, y = size_position.split('+')
-        w, h = size.split('x')
-        screens.append({
-            'w': w.split('/')[0],
-            'h': h.split('/')[0],
-            'x': x,
-            'y': y
-        })
-
-
-def get_active_window_title():
-    root = subprocess.Popen(['xprop', '-root', '_NET_ACTIVE_WINDOW'], stdout=subprocess.PIPE)
-    stdout, stderr = root.communicate()
-
-    m = re.search(b'^_NET_ACTIVE_WINDOW.* ([\w]+)$', stdout)
-    if m != None:
-        window_id = m.group(1)
-        window = subprocess.Popen(['xprop', '-id', window_id, 'WM_NAME'], stdout=subprocess.PIPE)
-        stdout, stderr = window.communicate()
-    else:
-        return None
-
-    match = re.match(b"WM_NAME\(\w+\) = (?P<name>.+)$", stdout)
-    if match != None:
-        return match.group("name").strip(b'"').decode()
-
-    return None
-
-
-def get_window():
-    window_name = get_active_window_title()
-    for line in _execute_command("wmctrl -p -G -l"):
-        values = list(filter(lambda i: i, line.split(' ')))
-        if len(values) > 6:
-            name = ' '.join(values[8:])
-            if window_name == name:
-                return values[0]
-
-
-def on_move(x, y):
-    global buttons, lock_x, lock_y, columns, lines, block_size_w, block_size_h, init_x, init_y, keys
-    ctrl = len(list(filter(lambda n: n == 'ctrl', keys))) > 0
-    shift = len(list(filter(lambda n: n == 'shift', keys))) > 0
-    if not ctrl:
-        return
-    right_clicked = len(list(filter(lambda bb: bb.value == 3, buttons))) == 1
-    if not right_clicked:
-        return
-    _break = False
-    block_x_init = 0
-    block_x_end = 0
-    block_y_init = 0
-    block_y_end = 0
-    for w_index in range(0, columns):
-        for h_index in range(0, lines):
-            block_x_init = init_x + block_size_w * w_index
-            block_x_end = block_x_init + block_size_w
-            block_y_init = init_y + block_size_h * h_index
-            block_y_end = block_y_init + block_size_h
-            if block_x_init <= x <= block_x_end and block_y_init <= y <= block_y_end:
-                _break = True
-                break
-        if _break:
-            break
-    window = get_window()
-    if shift:
-        window = get_window()
-        _execute_command("wmctrl -i -r " + window + " -b remove,maximized_vert,maximized_horz")
-        _execute_command(
-            "wmctrl -i -r " + window + " -e 0," + str(int(lock_x)) + "," + str(int(lock_y)) + "," + str(
-                int(block_x_end - lock_x)) + "," + str(int(block_y_end - lock_y)))
-    else:
-        if lock_x != block_x_init or lock_y != block_y_init:
-            lock_x = block_x_init
-            lock_y = block_y_init
-            _execute_command("wmctrl -i -r " + window + " -b remove,maximized_vert,maximized_horz")
-            _execute_command(
-                "wmctrl -i -r " + window + " -e 0," + str(int(lock_x)) + "," + str(int(lock_y)) + "," + str(
-                    int(block_size_w)) + "," + str(int(block_size_h)))
-
-
-def on_click(_x, _y, button, pressed):
+def _on_click(x, y, button, pressed):
     global buttons
-    if pressed:
-        buttons.append(button)
-    else:
-        buttons = list(filter(lambda _button: _button.value != button.value, buttons))
-
-
-def on_press(key):
-    global keys
-    try:
-        if key not in keys:
-            keys.append(key.name)
-    except:
-        pass
-
-
-def on_release(key):
-    global keys
-    try:
-        keys = list(filter(lambda k: k != key.name, keys))
-    except:
-        pass
+    buttons = on_click(x, y, button, pressed, buttons)
 
 
 def thread_mouse_click():
     from pynput.mouse import Listener
-    with Listener(on_click=on_click) as listener:
+    with Listener(on_click=_on_click) as listener:
         listener.join()
+
+
+def _on_move(x, y):
+    global last_x, last_y, last_command, last_timestamp, last_window, gui_roots
+    result = on_move(x, y, buttons, keyboard_keys, screens, last_x, last_y, last_command, last_timestamp, last_window,
+                     gui_roots)
+    if result:
+        last_x, last_y, last_command, last_timestamp, last_window = result
 
 
 def thread_mouse_move():
     from pynput.mouse import Listener
-    with Listener(on_move=on_move) as listener:
+    with Listener(on_move=_on_move) as listener:
         listener.join()
+
+
+def _on_press(key):
+    global keyboard_keys
+    keyboard_keys = on_press(key, keyboard_keys)
+
+
+def _on_release(key):
+    global keyboard_keys
+    keyboard_keys = on_release(key, keyboard_keys)
 
 
 def thread_keyboard():
     from pynput.keyboard import Listener
-    with Listener(on_press=on_press, on_release=on_release) as listener:
+    with Listener(on_press=_on_press, on_release=_on_release) as listener:
         listener.join()
 
 
 threading.Thread(target=thread_mouse_click).start()
 threading.Thread(target=thread_mouse_move).start()
 threading.Thread(target=thread_keyboard).start()
+gui_app.mainloop()
